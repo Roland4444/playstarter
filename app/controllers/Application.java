703347abadb09d7fba221efdde61592ca5103136
Roland4444 <turbopollux@rambler.ru>
2020-11-17 17:37:02 +0400

    good work with template render param

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,8 @@ import java.util.*;
 
 import models.*;
 
+import javax.swing.text.AbstractDocument;
+
 public class Application extends Controller {
 
     public static void index(){
@@ -15,8 +17,27 @@ public class Application extends Controller {
     public static void login(){
         render();
     }
-    public static String trylogin(String login, String password){
-        return login+"::"+password;
+    public static void trylogin(String login, String password){
+        //return login+"::"+password;
+
+        //views.html.Application.index.render(customer, orders);
+        renderArgs.put("somename", "customer" );
+        render();
     };
 
+    public static void test(){
+        Integer age = 22;
+        renderArgs.put("age", 55 );
+        render();
+    }
+
 }
+
+
+/////                       https://www.playframework.com/documentation/1.2.4/templates
+
+///// https://progi.pro/render-single-template-play-framework-7495835
+
+////https://askdev.ru/q/poluchit-stroku-tela-zaprosa-otpravlennuyu-v-post-request-v-play-framework-java-340221/
+
+////https://examples.javacodegeeks.com/enterprise-java/play-framework-path-query-and-route-default-params-example/
